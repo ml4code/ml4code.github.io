f36a0e8bd4d75485b0db7dee31c31553336ed0d3
Reza Gharibi <reza.gharibi.rg@gmail.com>
2024-08-20 08:05:57 +0330

    Update NLTK tokenizer's data

--- etc/compute_related.py
+++ etc/compute_related.py
@@ -6,7 +6,7 @@ import nltk
 
 nltk.download('stopwords')
 nltk.download('wordnet')
-nltk.download('punkt')
+nltk.download('punkt_tab')
 
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
